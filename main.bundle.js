d6362669bc6168d7a503425dbb1e680c321c8cdf
saschaabraham <34794615+saschaabraham@users.noreply.github.com>
2018-02-13 18:25:05 +0100

    1

--- main.bundle.js
+++ main.bundle.js
@@ -80,7 +80,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<amazon-login [clientId]=\"myClientId\" (amazonAuthResponse)=\"onAmazonAuthResponse($event)\">\n</amazon-login>\n\n<nav class=\"navbar navbar-inverse\">\n  <div class=\"container-fluid\">\n    <div class=\"navbar-header\">\n      <a class=\"navbar-brand\" href=\"#\">{{ title }} - Willkommen {{ userName }}</a>\n    </div>\n    <ul class=\"nav navbar-nav\">\n      <li class=\"active\" routerLinkActive=\"active\">\n        <a [routerLink]=\"['/home']\" [queryParamsHandling]=\"true\">Home</a>\n      </li>\n      <li routerLinkActive=\"active\">\n        <a [routerLink]=\"['/edit']\" [queryParamsHandling]=\"true\">Medikamenteneinstellungen</a>\n      </li>\n    </ul>\n  </div>\n</nav>\n\n\n\n\n\n\n<div class=\"container\">\n\n  <div class=\"row\">\n    <router-outlet></router-outlet>\n  </div>\n\n\n\n</div>"
+module.exports = "<div class=\"container-fluid\">\n  <div class=\"row\">\n    <div class=\"col-md-8 text-left\" *ngIf=\"loggedin; else elseBlock\"> {{ userName }}</div>\n    <ng-template #elseBlock>Bite melden Sie sich über den Amazon- Button an.</ng-template>\n    <div class=\"col-md-4\">\n      <amazon-login [clientId]=\"myClientId\" (amazonAuthResponse)=\"onAmazonAuthResponse($event)\">\n      </amazon-login>\n    </div>\n  </div>\n\n\n</div>\n\n\n<!-- <nav class=\"navbar navbar-inverse\">\n  <div class=\"container-fluid\">\n    <div class=\"navbar-header\">\n      <a class=\"navbar-brand\" href=\"#\">{{ title }} - Willkommen {{ userName }}</a>\n    </div>\n    <ul class=\"nav navbar-nav\">\n      <li class=\"active\" routerLinkActive=\"active\">\n        <a [routerLink]=\"['/home']\" [queryParamsHandling]=\"true\">Home</a>\n      </li>\n      <li routerLinkActive=\"active\">\n        <a [routerLink]=\"['/edit']\" [queryParamsHandling]=\"true\">Medikamenteneinstellungen</a>\n      </li>\n    </ul>\n  </div>\n</nav> -->\n\n\n"
 
 /***/ }),
 
@@ -91,7 +91,6 @@ module.exports = "<amazon-login [clientId]=\"myClientId\" (amazonAuthResponse)=\
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__amazonservices__ = __webpack_require__("../../../../../src/app/amazonservices.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -104,31 +103,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-
 var AppComponent = /** @class */ (function () {
-    function AppComponent(amService, router) {
-        var _this = this;
+    function AppComponent(amService) {
         this.amService = amService;
-        this.router = router;
         this.title = 'Pillenbox-Konfigurator';
         this.showWaitInfo = false;
         this.myClientId = 'amzn1.application-oa2-client.a9e64c41743f4d14813be90d8f38cf9f';
         this.userName = 'Nicht angemeldet';
         this.loggedin = false;
-        router.events.subscribe(function (event) {
-            if (event instanceof __WEBPACK_IMPORTED_MODULE_2__angular_router__["e" /* NavigationStart */]) {
-                _this.showWaitInfo = true;
-            }
-            if (event instanceof __WEBPACK_IMPORTED_MODULE_2__angular_router__["c" /* NavigationEnd */]
-                || event instanceof __WEBPACK_IMPORTED_MODULE_2__angular_router__["b" /* NavigationCancel */]
-                || event instanceof __WEBPACK_IMPORTED_MODULE_2__angular_router__["d" /* NavigationError */]) {
-                _this.showWaitInfo = false;
-            }
-        });
     }
-    AppComponent.prototype.goHome = function () {
-        this.router.navigate(['/home']);
-    };
     AppComponent.prototype.onAmazonAuthResponse = function (event) {
         var _this = this;
         if (event.authRequest.error === undefined) {
@@ -147,7 +130,7 @@ var AppComponent = /** @class */ (function () {
             styles: [__webpack_require__("../../../../../src/app/app.component.css")]
         }),
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__amazonservices__["a" /* Amazonservices */], __WEBPACK_IMPORTED_MODULE_2__angular_router__["f" /* Router */]])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__amazonservices__["a" /* Amazonservices */]])
     ], AppComponent);
     return AppComponent;
 }());
@@ -161,23 +144,21 @@ var AppComponent = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_angular_amazon_login__ = __webpack_require__("../../../../angular-amazon-login/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_angular_amazon_login___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_angular_amazon_login__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__home_home_component__ = __webpack_require__("../../../../../src/app/home/home.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__edit_edit_component__ = __webpack_require__("../../../../../src/app/edit/edit.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__amazonservices__ = __webpack_require__("../../../../../src/app/amazonservices.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__app_routes__ = __webpack_require__("../../../../../src/app/app.routes.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_animations__ = __webpack_require__("../../../platform-browser/esm5/animations.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_angular_amazon_login__ = __webpack_require__("../../../../angular-amazon-login/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_angular_amazon_login___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_angular_amazon_login__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__amazonservices__ = __webpack_require__("../../../../../src/app/amazonservices.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__angular_material__ = __webpack_require__("../../../material/esm5/material.es5.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-
 
 
 
@@ -190,195 +171,25 @@ var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
+        Object(__WEBPACK_IMPORTED_MODULE_2__angular_core__["NgModule"])({
             declarations: [
-                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_5__home_home_component__["a" /* HomeComponent */],
-                __WEBPACK_IMPORTED_MODULE_6__edit_edit_component__["a" /* EditComponent */],
-                __WEBPACK_IMPORTED_MODULE_2_angular_amazon_login__["AmazonLoginComponent"]
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_3_angular_amazon_login__["AmazonLoginComponent"]
             ],
             imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
-                __WEBPACK_IMPORTED_MODULE_3__angular_common_http__["b" /* HttpClientModule */],
-                __WEBPACK_IMPORTED_MODULE_8__app_routes__["a" /* AppRouterModule */]
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_animations__["a" /* BrowserAnimationsModule */],
+                __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_4__angular_common_http__["b" /* HttpClientModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_material__["a" /* MatButtonModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_material__["b" /* MatCheckboxModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_material__["c" /* MatMenu */]
             ],
-            providers: [__WEBPACK_IMPORTED_MODULE_7__amazonservices__["a" /* Amazonservices */],
+            providers: [__WEBPACK_IMPORTED_MODULE_6__amazonservices__["a" /* Amazonservices */],
             ],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
     return AppModule;
-}());
-
-
-
-/***/ }),
-
-/***/ "../../../../../src/app/app.routes.ts":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppRouterModule; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__home_home_component__ = __webpack_require__("../../../../../src/app/home/home.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__edit_edit_component__ = __webpack_require__("../../../../../src/app/edit/edit.component.ts");
-
-
-
-var APP_ROUTES = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: __WEBPACK_IMPORTED_MODULE_1__home_home_component__["a" /* HomeComponent */]
-    },
-    {
-        path: 'edit',
-        component: __WEBPACK_IMPORTED_MODULE_2__edit_edit_component__["a" /* EditComponent */]
-    },
-    {
-        path: '**',
-        redirectTo: 'home'
-    }
-];
-var AppRouterModule = __WEBPACK_IMPORTED_MODULE_0__angular_router__["g" /* RouterModule */].forRoot(APP_ROUTES /*,
- { preloadingStrategy: CustomPreloadingStrategy, useHash: false } */);
-// export let AppRouterModule = RouterModule.forRoot(APP_ROUTES, { useHash: true, enableTracing: true});
-
-
-/***/ }),
-
-/***/ "../../../../../src/app/edit/edit.component.css":
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
-// imports
-
-
-// module
-exports.push([module.i, "", ""]);
-
-// exports
-
-
-/*** EXPORTS FROM exports-loader ***/
-module.exports = module.exports.toString();
-
-/***/ }),
-
-/***/ "../../../../../src/app/edit/edit.component.html":
-/***/ (function(module, exports) {
-
-module.exports = "<p>\n  edit works!\n</p>\n"
-
-/***/ }),
-
-/***/ "../../../../../src/app/edit/edit.component.ts":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EditComponent; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-var EditComponent = /** @class */ (function () {
-    function EditComponent() {
-    }
-    EditComponent.prototype.ngOnInit = function () {
-    };
-    EditComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
-            selector: 'app-edit',
-            template: __webpack_require__("../../../../../src/app/edit/edit.component.html"),
-            styles: [__webpack_require__("../../../../../src/app/edit/edit.component.css")]
-        }),
-        __metadata("design:paramtypes", [])
-    ], EditComponent);
-    return EditComponent;
-}());
-
-
-
-/***/ }),
-
-/***/ "../../../../../src/app/home/home.component.css":
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
-// imports
-
-
-// module
-exports.push([module.i, "", ""]);
-
-// exports
-
-
-/*** EXPORTS FROM exports-loader ***/
-module.exports = module.exports.toString();
-
-/***/ }),
-
-/***/ "../../../../../src/app/home/home.component.html":
-/***/ (function(module, exports) {
-
-module.exports = "<div style=\"text-align:center\">\n  <h3>Home ist aktiv</h3>\n</div>"
-
-/***/ }),
-
-/***/ "../../../../../src/app/home/home.component.ts":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomeComponent; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__amazonservices__ = __webpack_require__("../../../../../src/app/amazonservices.ts");
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-
-
-
-var HomeComponent = /** @class */ (function () {
-    function HomeComponent(route, amService) {
-        this.route = route;
-        this.amService = amService;
-        this.AMS = amService;
-    }
-    HomeComponent.prototype.ngOnInit = function () {
-        this.route.params.subscribe(function (p) {
-            console.log(p);
-        });
-    };
-    HomeComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
-            selector: 'app-home',
-            template: __webpack_require__("../../../../../src/app/home/home.component.html"),
-            styles: [__webpack_require__("../../../../../src/app/home/home.component.css")]
-        }),
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* ActivatedRoute */], __WEBPACK_IMPORTED_MODULE_2__amazonservices__["a" /* Amazonservices */]])
-    ], HomeComponent);
-    return HomeComponent;
 }());
 
 
